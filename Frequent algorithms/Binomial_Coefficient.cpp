fc12766ad80dd2888ca37f38a20070e2407ad026
Satyam kant <satyamkant2805@gmail.com>
2022-09-04 18:46:47 +0530

    added Binomial_Coefficient

--- Frequent algorithms/Binomial_Coefficient.cpp
+++ Frequent algorithms/Binomial_Coefficient.cpp
@@ -5,20 +5,70 @@ using namespace std;
 #define int long long
 
 // number of ways to select R different elements from N different elements
-// ordere is not important
+// order is not important
+
+
+///////////////////////// in this NCR class N and R can be very large but MOD is small //////////////////////////
+class NCR {
+    int mod = 1000003;
+    vector<int> fac;
+    vector<int> fac_inv;
+    int power(int n, int m) {
+        int res = 1;
+        n %= mod;
+        while (m > 0) {
+            if (m & 1) {
+                res = (1LL * res * n) % mod;
+                m--;
+            }
+            else {
+                n = (1LL * n * n) % mod;
+                m /= 2;
+            }
+        }
+        return res % mod;
+    }
+    int fermat(int n, int r) {
+        n %= mod, r %= mod;
+        if (n < r) return 0;
+        if (r == 0 || n == r) return 1;
+        return (1LL * fac[n] * fac_inv[r] * fac_inv[n - r]) % mod;
+    }
+    long long lucas(long long n, long long r) {
+        if (r == 0) return 1;
+        int ni = n % mod, ri = r % mod;
+        return (lucas(n / mod, r / mod) * fermat(ni, ri)) % mod;
+    }
+
+public:
+    NCR() {
+        fac.resize(mod);
+        fac_inv.resize(mod);
+
+        fac[0] = 1;
+        for (int i = 1; i < mod; i++)
+            fac[i] = (1LL * fac[i - 1] * i) % mod;
+
+        fac_inv[mod - 1] = power(fac[mod - 1], mod - 2);
+        for (int i = mod - 2; i > 0; i--)
+            fac_inv[i] = (1LL * fac_inv[i + 1] * (i + 1)) % mod;
+    }
+
+    int nCr(long long n, long long r) {
+        return lucas(n, r);
+    }
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////// in this NCR N and R must not be very large ////////////////////////////////////
+class NCR {
 #define p 1000000007
 
-class NCR {
+    vector<int> fact;
+    vector<int> inv;
+    vector<int> invFact;
 
-    typedef long long ll;
-    int n = 100000;
-    int r = 500;
-
-    ll* fact = new ll[n]();
-    ll* inv = new ll[n]();
-    ll* invFact = new ll[n]();
-
-    void precompute() {
+    void precompute(int n) {
         fact[0] = 1;
 
         for (int i = 1; i <= n; i++) {
@@ -38,21 +88,26 @@ class NCR {
     }
 
 
-    ll computeCombinatorics(ll N, ll R) {
-        ll ans = (((fact[N] * invFact[R]) % p) * (invFact[N - R] % p)) % p;
+    int computeCombinatorics(int n, int r) {
+        int ans = (((fact[n] * invFact[r]) % p) * (invFact[n - r] % p)) % p;
         return ans;
     }
 
 public:
-    NCR() {
-        precompute();
+    NCR(int n) {
+        fact.resize(10001);
+        inv.resize(10001);
+        invFact.resize(10001);
+        precompute(n);
     }
 
-    int get_ncr(ll n, ll r) {
+    long long get_ncr(long long n, long long r) {
         return computeCombinatorics(n, r);
     }
 };
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int32_t main() {
+
     return 0;
 }
