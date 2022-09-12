c635921d645cfe8b8b75c57cca6f64270ca43bbd
Satyam kant <satyamkant2805@gmail.com>
2022-09-12 22:29:18 +0530

    updates

---
+++ Frequent algorithms/string_to_int.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+//////// function to convert string to integer //////////
+int conv(string s) {
+    int num;
+
+    stringstream temp(s);
+    temp >> num;
+
+    return num;
+
+}
+
+int main() {
+
+    return 0;
+
+}
